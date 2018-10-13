fc4640ef7c6a66e256e0287e93b0d434eb9ee916
Cedric Borreguín <cedricborreguin@Cedrics-MacBook-Pro.local>
2018-10-13 18:18:03 -0500

    Modify ReceiptProductTable

--- src/com/borreguin/cafeteriapointofsale/DBHelper/ReceiptProductTable.java
+++ src/com/borreguin/cafeteriapointofsale/DBHelper/ReceiptProductTable.java
@@ -1,4 +1,19 @@
 package com.borreguin.cafeteriapointofsale.DBHelper;
 
 public class ReceiptProductTable {
+    public static final String RECEIPT_PRODUCT = "receipt_product";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_PRODUCT = "product_id";
+    public static final String COLUMN_QUANTITY = "quantity";
+    public static final String[] ALL_COLUMNS =
+            {COLUMN_ID, COLUMN_PRODUCT, COLUMN_QUANTITY};
+
+    public static final String SQL_CREATE =
+            "CREATE TABLE " + RECEIPT_PRODUCT + "(" +
+                    COLUMN_ID + " TEXT PRIMARY KEY," +
+                    COLUMN_PRODUCT + " TEXT," +
+                    COLUMN_QUANTITY + " INTEGER" + ");";
+
+    public static final String SQL_DELETE =
+            "DROP TABLE " + RECEIPT_PRODUCT;
 }
